72e1096b356a28014dcb1f1fc3ec8df983222797
Coconut <coconutat@protonmail.com>
2023-07-06 18:25:22 +0800

    Update user_build_Kernel.sh

--- user_build_Kernel.sh
+++ user_build_Kernel.sh
@@ -39,6 +39,8 @@ if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing kernel...***"
 
+	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on ssbd=kernel printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing unmovable_isolate1=2:192M,3:224M,4:256M buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01  --output Kirin980-${date}.img
+
 	tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on ssbd=kernel printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=permissive unmovable_isolate1=2:192M,3:224M,4:256M buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x07c00000 --header_version 1 --os_version 9 --os_patch_level 2019-03-01  --output Kirin980_PM-${date}.img
 
 	cp out/arch/arm64/boot/Image.gz Image.gz 
