88d85ee1cdf4e678322dcc92656ce1ab0cee0407
Coconut <coconutat@protonmail.com>
2023-07-07 11:39:12 +0800

    Create pack_kernerimage_cmd_Mate20.sh

---
+++ tools/pack_kernerimage_cmd_Mate20.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./mkbootimg --kernel kernel --base 0x0 --cmdline "loglevel=4 initcall_debug=n page_tracker=on ssbd=kernel printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing unmovable_isolate1=2:192M,3:224M,4:256M buildvariant=user" --tags_offset 0x07988000 --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --header_version 1 --os_version 9.0.0 --os_patch_level 2019-08  --output kernel.img
